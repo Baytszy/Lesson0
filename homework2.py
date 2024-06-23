5cabc23004ee3e57f8136cc6f68e3c878ef0b983
Katya <86482797+Baytszy@users.noreply.github.com>
2024-06-23 15:06:39 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+# переменные
+
+numberHomeWork = 12
+numberHoursSpent = 1.5
+courseName = "Python"
+timesOneTask = numberHoursSpent / numberHomeWork
+print("Cours: ", courseName)
+print("всего задач: ", numberHomeWork)
+print("затрачено часов: ", numberHoursSpent)
+print("среднее время выполнения: ", timesOneTask)
