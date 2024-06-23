ee7c3f64c37bf97c5cb7c8cf897b3c3d4bda5879
Katya <86482797+Baytszy@users.noreply.github.com>
2024-06-23 14:12:35 +0300

    Add files via upload

---
+++ les001.py
@@ -0,0 +1,13 @@
+#name = "Denis"
+#print(name[0:3:2]) #начало конец шаг
+#print(name[:3]) # до трех эл
+#print(name[2:]) #с второго шага до конца
+#print(name[::-2]) # пропуск начала и конца среза только шаг
+
+example  = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[:5])
+print((example[::-1]))
+print((example[1::2]))
+
